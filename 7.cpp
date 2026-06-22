39004bef2005463c9c462e00383a93bd50a167b3
Suryansh Pandey <suryanshpandey9920@gmail.com>
2026-06-23 00:33:42 +0530

    New Problem for character Printing using loops

---
+++ 7.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+// A
+// B B
+// C C C    (Character pattern problem)
+
+int main(){
+
+    int n ;
+    cout << "Enter the value of n : ";
+    cin >> n;
+
+    char ch = 'A';
+
+    for (int i =0 ;i< n ; i++){  // Outer loop for line
+
+            for (int j =0; j<i+1; j++){  //Inner loop for Character printing 
+                
+                cout << ch  << " ";
+                // Every line CH get print (i+1) times 
+            }
+            ch++;
+            // ch get plus after printing in one line  
+
+        cout << endl;
+        
+    }
+
+    return 0;
+}
