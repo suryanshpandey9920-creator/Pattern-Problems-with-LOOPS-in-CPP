61216ad41363d768799588110800bb2e591b13d4
Suryansh Pandey <suryanshpandey9920@gmail.com>
2026-06-29 02:53:52 +0530

    Reverse elements of right angle triangle  Pattern Problem

--- 12.cpp
+++ 12.cpp
@@ -3,9 +3,9 @@ using namespace std ;
 
 int main() {
     // 1
-    // 1 2
-    // 1 2 3 
-    // 1 2 3 4 
+    // 2 1 
+    // 3 2 1  
+    // 4 3 2 1  
  // Reverse triangle pattern
      int n;
      cout << "enter the value of n : ";
