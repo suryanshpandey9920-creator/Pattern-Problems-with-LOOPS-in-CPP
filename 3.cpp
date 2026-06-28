cc3eac7bb165bb9382dd4d862983c9e589288010
Suryansh Pandey <suryanshpandey9920@gmail.com>
2026-06-28 15:31:03 +0530

    Right angled Triangle (star) Pattern Problem

--- 3.cpp
+++ 3.cpp
@@ -16,13 +16,11 @@ int main(){
     for (int i =0; i<=n-1; i++){  // outer loop for N number of lines 
 
         for (int j=0; j<=i; j++){   
-
             //   i + 1 time print values im each row 
                   //   1st row -> ( i= 0 )+1
                   //   2nd row -> ( i= 1 )+1 
                   //   3rd row -> ( i= 2 )+2
                   //  for all the row's 
-
             cout << "* ";
         }
         cout << endl;
