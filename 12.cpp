8c16cfdefdf3b9d08002a4b25b05b400cd51ce70
Suryansh Pandey <suryanshpandey9920@gmail.com>
2026-06-28 15:24:16 +0530

    Reverse Pattern problem

--- 12.cpp
+++ 12.cpp
@@ -11,8 +11,8 @@ int main() {
      cout << "enter the value of n : ";
      cin >> n; 
 
-     for (int i =0; i<=n ; i++){
-        for (int j = i ; j>0 ; j--){
+     for (int i =0; i<=n ; i++){   // Outer Loop
+        for (int j = i ; j>0 ; j--){ // Inner Reverse Loop
             cout << j;
             
         }
